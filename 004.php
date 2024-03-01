2bcd2194f7882dcd8127e0baea661ac938bea9e5
Paula Fernanda da Silva <71718229+paulafernandas@users.noreply.github.com>
2024-03-01 19:14:50 -0300

    Add files via upload

---
+++ 004.php
@@ -0,0 +1,35 @@
+<?php
+
+ $notaA = 9;
+ $notaB = 3;
+ $mediaDesejada = 7;
+
+ $media = ($notaA+$notaB)/2;
+
+ if ($media>= $mediaDesejada){
+     echo "Parabéns! Você está aprovado!";
+ }else{
+     echo "Sinto muito, mas você está de recuperação.";
+ }
+
+ $usuario = "paula_fernanda";
+ $senha= 12345;
+
+ $usuarioCorreto = "Operador01";
+ $senhaCorreta = 123;
+
+ if ($usuario == $usuarioCorreto && $senha == $senhaCorreta){
+     echo "Login realizado com sucesso!";
+ } else {
+     echo "Login inválido!";
+ }
+
+$valor= 200;
+
+if ($valor>=100){
+    $desconto = $valor *0.1;
+    $valorFinal = $valor - $desconto;
+    echo "Desconto aplicado: $valorFinal";
+} else {
+    echo "Sua compra não atinge o valor minímo para desconto";
+}
